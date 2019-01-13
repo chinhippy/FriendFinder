95110e256c8bc3192fef1ed903b33a59b8960371
Anton Kogan <34128946+chinhippy@users.noreply.github.com>
2019-01-13 15:51:00 -0600

    server created and node modules installed

--- server.js
+++ server.js
@@ -5,4 +5,18 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing
+// ---------------------
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
+// Require necessary routing files
+// ----------------------
+require('.app/routing/apiRoutes')(app)
+require('.app/routing/htmlRoutes')(app)
+
+// Starts the server to begin listening
+// ----------------------
+app.listen(PORT, function() {
+    console.log(`App listening on ${PORT}`)
+})
